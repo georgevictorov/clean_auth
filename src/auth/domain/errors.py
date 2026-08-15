8b62250b04121d62093b2cb315f67b37646cfe1c
georgiiviktorov <nowayisleft@gmail.com>
2026-08-15 11:43:03 +0700

    fix: fix errors

--- src/auth/domain/errors.py
+++ src/auth/domain/errors.py
@@ -1,8 +1,17 @@
 class DomainError(Exception):
+    """Base class for domain errors."""
     ...
 
 
+class InfrastructureError(Exception):
+    """Infrastructure failure."""
+    ...
+
+
+# Validation
+
 class ValidationError(DomainError):
+    """Invalid domain value."""
     ...
 
 
@@ -26,17 +35,34 @@ class InvalidCreationTime(ValidationError):
     ...
 
 
+# Authentication
+
 class TokenDecodeError(DomainError):
+    """Token cannot be decoded or validated."""
     ...
 
 
 class InvalidCredentials(DomainError):
+    """Provided credentials are invalid."""
     ...
 
+
+# User
 
 class UserAlreadyExists(DomainError):
     ...
 
 
 class UserNotFound(DomainError):
+    ...
+
+
+# Concurrency
+
+class ConflictError(DomainError):
+    """Domain conflict."""
+    ...
+
+
+class ConcurrencyError(ConflictError):
     ...
